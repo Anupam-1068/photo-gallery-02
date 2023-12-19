96d72b22928d75588833c815ec5a5dd6f675cbb5
Anupam-1068 <107940692+Anupam-1068@users.noreply.github.com>
2023-12-19 11:19:39 +0200

    Gallery app with better UI

--- lib/screens/gallery_grid.dart
+++ lib/screens/gallery_grid.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 import '../controllers/photo_controller.dart';
 import '../models/photo.dart';
 import 'details_screen.dart';
@@ -9,51 +11,96 @@ class GalleryGrid extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Photo Gallery'),
-      ),
-      body: FutureBuilder<List<Photo>>(
-        future: _photoController.fetchPhotos(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
-
-          if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          }
-
-          final List<Photo> photos = snapshot.data ?? [];
-
-          return GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 8.0,
-              mainAxisSpacing: 8.0,
+      backgroundColor: Colors.lightBlueAccent,
+      // appBar: AppBar(
+      //   title: Text('Photo Gallery'),
+      // ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            SizedBox(
+              height: 40,
             ),
-            itemCount: photos.length,
-            itemBuilder: (BuildContext context, int index) {
-              final photo = photos[index];
-
-              return GestureDetector(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DetailsScreen(photo: photo),
-                    ),
-                  );
-                },
-                child: Image.network(
-                  photo.imageUrl,
-                  fit: BoxFit.cover,
+            Text(
+              'Gallery',
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 30,
                 ),
-              );
-            },
-          );
-        },
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: FutureBuilder<List<Photo>>(
+                  future: _photoController.fetchPhotos(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(child: CircularProgressIndicator());
+                    }
+
+                    if (snapshot.hasError) {
+                      return Center(child: Text('Error: ${snapshot.error}'));
+                    }
+
+                    final List<Photo> photos = snapshot.data ?? [];
+
+                    return GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: 10,
+                      ),
+                      itemBuilder: (context, index) {
+                        final photo = photos[index];
+
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DetailsScreen(photo: photo),
+                              ),
+                            );
+                          },
+                          child: Hero(
+                            tag: 'photoHero${photo.imageUrl}',
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                image: DecorationImage(
+                                  image: NetworkImage(photo.imageUrl),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      itemCount: photos.length,
+                    );
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
 }
-
